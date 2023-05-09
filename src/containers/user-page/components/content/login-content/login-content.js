2439f2d5ab4e90a431564400c62712871a5cdfb9
YapieRT <sokolval555@gmail.com>
2023-05-09 21:29:25 +0300

    Update for Login and Reg

--- src/containers/user-page/components/content/login-content/login-content.js
+++ src/containers/user-page/components/content/login-content/login-content.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import './login-content.css';
 
+import { useState } from 'react';
+import axios from 'axios';
+
 const LoginContent = () => {
-    return (
-        <div className="content">
-            <h2>Login</h2>
-            <form>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Log In</button>
-            </form>
-        </div>
-    );
+  const [password, setEnteredPassword] = useState('');
+  const [email, setEnteredEmail] = useState('');
+  const [loginStatus, setLoginStatus] = useState('');
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const postData = { email: email, password: password };
+    await axios
+      .post('http://localhost:3002/login', postData)
+      .then((response) => {
+        setLoginStatus(response.data.message);
+      })
+      .catch((error) => {
+        setLoginStatus(error.response.data.message);
+      });
+  };
+  return (
+    <div className='content'>
+      <h2>Login</h2>
+      <form onSubmit={submitHandler}>
+        <input required type='text' placeholder='Email' onChange={emailChangeHandler} />
+        <input required type='password' placeholder='Password' onChange={passwordChangeHandler} />
+        <p>{loginStatus}</p>
+        <button className='loginButton' type='submit'>
+          Log In
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginContent;
--- src/containers/user-page/components/content/reg-content/reg-content.js
+++ src/containers/user-page/components/content/reg-content/reg-content.js
@@ -1,42 +1,229 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
 import './reg-content.css';
-import { RegistrationStepOne, RegistrationStepTwo, RegistrationStepThree } from './step-list/reg-steps';
+import { useState } from 'react';
 
 const RegistrationContent = () => {
-    const regComp = RegistrationStepOne();
-    return (
-        <Carousel variant="dark">
-            <Carousel.Item>
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    birthDate: '',
+    phone: '',
+    address: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    color: '',
+  });
 
-                <RegistrationStepOne />
-                <Carousel.Caption>
-                    <h5>Third slide label</h5>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <RegistrationStepTwo />
-                <Carousel.Caption>
-                    <h5>Third slide label</h5>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <RegistrationStepThree />
-                <Carousel.Caption>
-                    <h5>Third slide label</h5>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
-    );
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleNext = () => {
+    setStep((prevStep) => prevStep + 1);
+  };
+
+  const handlePrevious = () => {
+    setStep((prevStep) => prevStep - 1);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Process the form data
+    console.log(formData);
+  };
+
+  return (
+    <div className='registration-form'>
+      <div className='step-progress'>
+        <div className={`step ${step === 1 ? 'active' : ''}`}></div>
+        <div className={`step ${step === 2 ? 'active' : ''}`}></div>
+        <div className={`step ${step === 3 ? 'active' : ''}`}></div>
+        <div className={`step ${step === 4 ? 'active' : ''}`}></div>
+      </div>
+      <form onSubmit={handleSubmit}>
+        {step === 1 && (
+          <div className='step-content'>
+            <button type='button' onClick={handleNext}>
+              Start
+            </button>
+          </div>
+        )}
+        {step === 2 && (
+          <div className='step-content'>
+            <div className='row'>
+              <label>First Name:</label>
+              <input
+                type='text'
+                name='firstName'
+                value={formData.firstName}
+                onChange={handleChange}
+                placeholder='First Name'
+                required
+              />
+              <label>Last Name:</label>
+              <input
+                type='text'
+                name='lastName'
+                value={formData.lastName}
+                onChange={handleChange}
+                placeholder='Last Name'
+                required
+              />
+            </div>
+            <div className='row'>
+              <label>Birth Date:</label>
+              <input
+                type='date'
+                name='birthDate'
+                value={formData.birthDate}
+                onChange={handleChange}
+                placeholder='Birth Date'
+                required
+              />
+              <label>Phone:</label>
+              <input
+                type='tel'
+                name='phone'
+                value={formData.phone}
+                onChange={handleChange}
+                placeholder='Phone'
+                required
+              />
+            </div>
+
+            <div className='row'>
+              <div className='address'>
+                <label>Adress:</label>
+                <input
+                  type='text'
+                  name='address'
+                  value={formData.address}
+                  onChange={handleChange}
+                  placeholder='Address'
+                  required
+                />
+              </div>
+            </div>
+            <div className='row'>
+              <div className='address'>
+                <label>Email:</label>
+                <input
+                  type='email'
+                  name='email'
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder='Email'
+                  required
+                />
+              </div>
+            </div>
+            <div className='row'>
+              <label>Password:</label>
+              <input
+                type='password'
+                name='password'
+                value={formData.password}
+                onChange={handleChange}
+                placeholder='Password'
+                required
+              />
+              <label>Confirm Password:</label>
+              <input
+                type='password'
+                name='confirmPassword'
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                placeholder='Confirm Password'
+                required
+              />
+            </div>
+            <div className='button-container'>
+              <button className='previous' type='button' onClick={handlePrevious}>
+                Previous
+              </button>
+              <button type='button' onClick={handleNext}>
+                Next
+              </button>
+            </div>
+          </div>
+        )}
+        {step === 3 && (
+          <div className='step-content'>
+            <div className='row'>
+              <div className='input-container'>
+                <label>Color:</label>
+                <div className='radio-container'>
+                  <input
+                    type='radio'
+                    id='yellow'
+                    name='color'
+                    value='yellow'
+                    checked={formData.color === 'yellow'}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor='yellow'>Yellow</label>
+                </div>
+                <div className='radio-container'>
+                  <input
+                    type='radio'
+                    id='red'
+                    name='color'
+                    value='red'
+                    checked={formData.color === 'red'}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor='red'>Red</label>
+                </div>
+                <div className='radio-container'>
+                  <input
+                    type='radio'
+                    id='pink'
+                    name='color'
+                    value='pink'
+                    checked={formData.color === 'pink'}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor='pink'>Pink</label>
+                </div>
+                <div className='radio-container'>
+                  <input
+                    type='radio'
+                    id='purple'
+                    name='color'
+                    value='purple'
+                    checked={formData.color === 'purple'}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor='purple'>Purple</label>
+                </div>
+              </div>
+            </div>
+            <div className='button-container'>
+              <button className='previous' type='button' onClick={handlePrevious}>
+                Previous
+              </button>
+              <button type='button' onClick={handleNext}>
+                Next
+              </button>
+            </div>
+          </div>
+        )}
+        {step === 4 && (
+          <div className='step-content'>
+            <button className='previous' type='button' onClick={handlePrevious}>
+              Previous
+            </button>
+            <button type='submit'>Submit</button>
+          </div>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default RegistrationContent;
